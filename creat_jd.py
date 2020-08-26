f6474b5adf21ab5a066362286e6a81e204d2005e
songshijun007 <jiaxiong_greenzone@163.com>
2020-08-26 18:19:04 +0800

    Update creat_jd.py

--- creat_jd.py
+++ creat_jd.py
@@ -13,13 +13,41 @@ origin_repo = g.get_repo("songshijun007/scripts")
 target_repo = g.get_repo("songshijun007/jd_by_api")
 
 contents = origin_repo.get_contents('')
+workflows_contents = origin_repo.get_contents('.github/workflows')
+icon_contents = origin_repo.get_contents('icon')
 
-while contents:
-    file_content = contents.pop(0)
-    if file_content.type == "dir":
-        contents.extend(origin_repo.get_contents(file_content.path))
-    else:
-        print(file_content)
-        target_repo.create_file(path=file_content.path, 
-                                content=base64.b64decode(file_content.content).decode("utf-8"), 
-                                message="create")
+# while contents:
+#     file_content = contents.pop(0)
+#     if file_content.type == "dir":
+#         contents.extend(origin_repo.get_contents(file_content.path))
+#     else:
+#         print(file_content)
+#         target_repo.create_file(path=file_content.path, 
+#                                 content=base64.b64decode(file_content.content).decode("utf-8"), 
+#                                 message="create")
+for content_ in contents:
+    try:
+        if content_.content:
+            target_repo.create_file(path=content_.path, 
+                                    content=base64.b64decode(content_.content).decode("utf-8"), 
+                                    message="create")
+    except Exception as e:
+        print(e)
+
+for content_ in workflows_contents:
+    try:
+        if content_.content:
+            target_repo.create_file(path=content_.path, 
+                                    content=base64.b64decode(content_.content).decode("utf-8"), 
+                                    message="create")
+    except Exception as e:
+        print(e)
+        
+for content_ in icon_contents:
+    try:
+        if content_.content:
+            target_repo.create_file(path=content_.path, 
+                                    content=base64.b64decode(content_.content).decode("utf-8"), 
+                                    message="create")
+    except Exception as e:
+        print(e)
